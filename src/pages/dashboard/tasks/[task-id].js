028e756862f5bd76aa80b221df7a5a462d22087b
sidp919 <sidp0008@gmail.com>
2023-04-05 00:37:24 +0530

    Part-03.1-SSR-getServerSideProps-function-in-NextJS

--- src/pages/dashboard/tasks/[task-id].js
+++ src/pages/dashboard/tasks/[task-id].js
@@ -1,18 +1,73 @@
 import {useRouter} from 'next/router'
 
-const TaskDetails = () => {
+const TaskDetails = ({tasks}) => {
+    
     const router = useRouter();
-    console.log(router.query["task-id"])
+    console.log("router.query['task-id'] = ",router.query["task-id"])
+    const taskId = router.query["task-id"]
+    
+    const task = tasks.filter((task) => {
+        if(task.taskId === taskId){
+            return true;
+        }else return false;
+    })[0];
+    
     return(
         <div>
             <h1>Task Details</h1>
+            {
+                (task)?(
+                    <div>
+                        <br/>
+                        <div><h3>Task Id: {task.taskId}</h3></div>
+                        <div><h3>Task Title: {task.taskTitle}</h3></div>
+                        <div><h3>Task Description: {task.taskDescription}</h3></div>
+                    </div>
+                ):(
+                    <div><br/><h3>Invalid Task Id: {taskId}</h3></div>
+                )
+            }
         </div>
     )
 }
 
+export async function getServerSideProps(context){
+
+    console.log("context.params = ",context.params) // Sample O/P: context.params =  { 'task-id': '00001' }
+    console.log("context.params['task-id'] = ",
+                    context.params['task-id']) // Sample O/P: context.params['task-id'] =  00001
+    
+    return {
+        props: {
+            tasks:[
+                {
+                    taskId: '00001',
+                    taskTitle: 'Task 01',
+                    taskDescription: 'Not available',
+                },
+                {
+                    taskId: '00002',
+                    taskTitle: 'Task 02',
+                    taskDescription: 'Not available',
+                },
+                {
+                    taskId: '00003',
+                    taskTitle: 'Task 03',
+                    taskDescription: 'Not available',
+                },
+            ]
+        }
+    };
+}
+// Above function getServerSideProps() is useful whenever we have some component which
+// needs to fetch data from the server-side or some remote source quite frequently
+// (or in another sense, when we have some data that changes quite frequently)
+// and based on the data recieved, needs to make changes on frontend-UI dynamically.
+
+// In above function, we have assumed that we received tasks array in response from backend
+
 export default TaskDetails;
-// So, if you try to hit this url- http://localhost:3000/dashboard/tasks/21 
-// Then, Output in console will be: 21
-// Also, notice how above code is an example of Client-side rendering(not exactly),
-// where we are using router to fetch the id from the query part of the request(not really),
-// and perform some function based on that id(in our case, we are logging id in console).
+
+// So, if you try to hit this url- http://localhost:3000/dashboard/tasks/00003 
+// Then, Output in console will be: 00003
+// However, on browser window, we wil see the details of task with task-id = '00003'
--- src/pages/dashboard/tasks/index.js
+++ src/pages/dashboard/tasks/index.js
@@ -1,15 +1,61 @@
-const Tasks = () => {
+const Tasks = (props) => { // This page will be executed on Client-side
     return (
         <div>
             <h1>Tasks</h1>
             <ul>
-                <li>Task 01</li>
-                <li>Task 02</li>
-                <li>Task 03</li>
+                <li>Task-Id &nbsp;Task-Title</li>
+                {props.tasks.map((task)=>{
+                    return(
+                        <li key={task.taskId}>{task.taskId} - &nbsp;{task.taskTitle}</li>
+                    )
+                })}
             </ul>
         </div>
     )
 }
+
+export async function getServerSideProps(context){ // But this function can/will be executed on the Server-side only
+
+    // Below log expressions won't be logged on our browser console bcoz browser works on the client-side
+    // whereas below expressions will be executed only on the server side
+    // So, we need to check in the terminal window where we are running our app-server.
+    console.log("context = ",context); // O/P: A big object with dozens of details including below ones.
+    console.log("context.query = ", context.query); // O/P: Context.query =  {}
+    console.log("context.params = ",context.params) // O/P: context.params =  undefined
+    console.log("context.req.url = ",
+                    context.req.url); // O/P: Context.req.url =  /_next/data/development/dashboard/tasks.json
+    console.log("context.resolvedUrl = ",context.resolvedUrl); // O/P: Context.resolvedUrl =  /dashboard/tasks
+    console.log("context.req.method = ",context.req.method); // Context.req.method =  GET
+
+    return {
+        props: {
+            tasks:[
+                {
+                    taskId: '00001',
+                    taskTitle: 'Task 01',
+                    taskDescription: 'Not available',
+                },
+                {
+                    taskId: '00002',
+                    taskTitle: 'Task 02',
+                    taskDescription: 'Not available',
+                },
+                {
+                    taskId: '00003',
+                    taskTitle: 'Task 03',
+                    taskDescription: 'Not available',
+                },
+            ]
+        }
+    };
+}
+
+// Above function getServerSideProps() is useful whenever we have some component which
+// needs to fetch data from the server-side or some remote source quite frequently
+// (or in another sense, when we have some data that changes quite frequently)
+// and based on the data recieved, needs to make changes on frontend-UI dynamically.
+
+// In above function, we have assumed that we received tasks array in response from backend
 
 export default Tasks;
 // Go to http://localhost:3000/dashboard/tasks to check/validate changes made in this file.
