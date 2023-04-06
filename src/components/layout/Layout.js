42becf12821f8d867d0987378fd10f006007bda5
sidp919 <sidp0008@gmail.com>
2023-04-06 20:15:22 +0530

    Part-07.01-Adding-Google-Fonts-To-Pages-In-NextJS-13

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,19 @@
 import Footer from '../home/Footer';
 import Header from '../home/Header';
+import {Roboto_Mono} from 'next/font/google'
+
+const robotoMono = Roboto_Mono({
+  weight:['200','400', '700'],
+  style:['italic'],
+  display:'swap',
+  subsets: ['latin']
+})
 
 const Layout = ({children}) => {
     return(
         <>
         <Header/>
-        <main style={{paddingTop: '50px'}}>
+        <main style={{paddingTop: '50px'}} className={robotoMono.className}>
             {children}
         </main>
         <Footer/>
