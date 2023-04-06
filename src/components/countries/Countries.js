4f70b613f20709e18b461487c24485a01f3ee3c0
sidp919 <sidp0008@gmail.com>
2023-04-07 01:23:03 +0530

    Part-08-Client-Side-Search-Functionality-In-NextJS

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,18 +1,44 @@
 import Head from 'next/head';
 import styles from './../../styles/countries.module.scss'
+import { useState } from 'react';
 const Countries = ({countryNames}) => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchResult, setSearchResult] = useState(countryNames);
+    const handleChange = (evt) => {
+        setSearchQuery(evt.target.value)
+        if(searchQuery.length>2){
+            setSearchResult(countryNames.filter((country)=>{
+                if(country.indexOf(searchQuery.trim())===-1)
+                    return false;
+                else return true;
+            }));
+        }else if(searchQuery.length===0){
+            setSearchResult(countryNames)
+        }
+    }
+
     return(
         <div>
             <Head>
                 <title>Countries</title>
             </Head>
             <div className={styles.countryList}>
-                <h1 className='pl-5'>Countries List</h1>
+                <div className='px-4 flex justify-between flex-wrap'>
+                    <h1 className='inline md:w-1/2'>Countries List</h1>
+                    <div className='mt-5 w-full md:w-1/2 md:text-end text-center mx-auto'>
+                        <input type='text' placeholder='Start typing a Country name'
+                            className='w-[60%] min-w-[240px] mx-auto md:w-3/5 h-[32px] py-6 px-2 rounded-md overflow-hidden break-words text-black'
+                            value={searchQuery}
+                            onChange={handleChange}
+                            onKeyUp = {handleChange}
+                        />
+                    </div>
+                </div>
                 <p className='text-xl text-white font-bold pl-5 pb-2 hover:skew-x-12 duration-300'>(Click on Countries' name to see their details)</p>
                 <div>
                     <br/>
                     <ul>
-                        {countryNames?.map(
+                        {searchResult?.map(
                             (country,i)=>(
                                 <li 
                                 key={i}
